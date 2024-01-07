90628298846a5f784f9ac4a268e5ca223c3880ae
Joebi-dev <jbezerra318.jh@gmail.com>
2024-01-07 16:08:06 -0300

    main

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,36 @@
 let tik = true
+let vitorias = []
+let vez
 
-function jogada(index) {
-    const button = document.getElementsByClassName("campo")[index]
-    
-    if (button.textContent == "") {
-        console.log(index)
-        button.textContent = tik ? 'X' : 'O'
-        button.style.color = tik ? 'red' : 'blue'
-        
-        tik = !tik
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('carreguei o documents')
+    const jogo = document.getElementById('jogo')
+    vez = document.getElementById('vez')
+    vez.textContent = 'X'
+    for (let i = 0; i < 9; i++) {
+        const campo = document.createElement('button')
+        campo.classList.add('campo')
+        campo.addEventListener("click", () => jogada(i))
+        jogo.appendChild(campo)
     }
 
 
-}
+})
+
+function jogada(index) {
+    const button = document.getElementsByClassName("campo")[index]
+    if (button.textContent == "") {
+        if (tik) {
+            vez.textContent = 'O'
+            vez.style.color = 'blue'
+            button.textContent = 'X'
+            button.style.color = 'red'
+        } else {
+            vez.textContent = 'X'
+            vez.style.color = 'red'
+            button.textContent = 'O'
+            button.style.color = 'blue'
+        }
+        tik = !tik
+    }
+} 
