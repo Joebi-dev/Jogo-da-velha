d48b4f6c0c153c7bb10c900da5f6864e5de39b43
Joebi-dev <jbezerra318.jh@gmail.com>
2024-01-09 04:03:00 -0300

    funcional

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,28 +1,23 @@
 let tik = true
-let vitorias = []
 let vez
+let jogando = true
 let tabuleiro = ['', '', '', '', '', '', '', '', ''];
 
-
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('carreguei o documents')
     const jogo = document.getElementById('jogo')
     vez = document.getElementById('vez')
-    vez.textContent = 'X'
     for (let i = 0; i < 9; i++) {
         const campo = document.createElement('button')
         campo.classList.add('campo')
         campo.addEventListener("click", () => jogada(i))
-        campo.setAttribute('title','Clique para jogar')
+        campo.setAttribute('title', 'Clique para jogar')
         jogo.appendChild(campo)
     }
-
-
 })
 
 function jogada(index) {
     const button = document.getElementsByClassName("campo")[index]
-    if (button.textContent == "") {
+    if (button.textContent == "" && jogando == true) {
         if (tik) {
             vez.style.color = 'blue'
             vez.textContent = 'O'
@@ -34,23 +29,25 @@ function jogada(index) {
             button.textContent = 'O'
             button.style.color = 'blue'
         }
-        
         tik = !tik
         tabuleiro[index] = (button.textContent)
         verificarVencedor()
     }
-} 
+}
 
-function limpar(seletor) {
+function limpar(select) {
     try {
-        const array = document.querySelectorAll(seletor)
         tik = true
+        jogando = true
         vez.textContent = 'X'
         vez.style.color = 'red'
-        array.forEach(el => {
-            if (el.textContent != '') {
-                el.textContent = ''
+        const array = document.querySelectorAll(select)
+            for (let i = 0; i < tabuleiro.length; i++) {
+                tabuleiro[i] = ''
             }
+        array.forEach(el => {
+            (!isNaN(el.textContent) && el.textContent != '') ? el.textContent = '0' : el.textContent = ''
+
         });
     } catch (error) {
         console.log(error)
@@ -72,11 +69,11 @@ function verificarVencedor() {
     for (let linha of linhasVencedoras) {
         const [a, b, c] = linha;
         if (tabuleiro[a] !== '' && tabuleiro[a] === tabuleiro[b] && tabuleiro[a] === tabuleiro[c]) {
-            console.log(tabuleiro[a])// Retorna 'X' ou 'O' (o jogador vencedor)
+            jogando = false
+            const vencedor = document.getElementById(tabuleiro[a].toLowerCase())
+            vencedor.textContent++
         }
     }
-
-    // return null; // Retorna null se não houver vencedor
 }
 
 
