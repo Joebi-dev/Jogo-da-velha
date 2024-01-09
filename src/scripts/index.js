04d4cf244abbb56849b13461968913c8c2c11f8e
Joebi-dev <jbezerra318.jh@gmail.com>
2024-01-09 02:40:33 -0300

    foi

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,8 @@
 let tik = true
 let vitorias = []
 let vez
+let tabuleiro = ['', '', '', '', '', '', '', '', ''];
+
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('carreguei o documents')
@@ -22,16 +24,59 @@ function jogada(index) {
     const button = document.getElementsByClassName("campo")[index]
     if (button.textContent == "") {
         if (tik) {
-            vez.textContent = 'O'
             vez.style.color = 'blue'
+            vez.textContent = 'O'
             button.textContent = 'X'
             button.style.color = 'red'
         } else {
-            vez.textContent = 'X'
             vez.style.color = 'red'
+            vez.textContent = 'X'
             button.textContent = 'O'
             button.style.color = 'blue'
         }
+        
         tik = !tik
+        tabuleiro[index] = (button.textContent)
+        verificarVencedor()
     }
 } 
+
+function limpar(seletor) {
+    try {
+        const array = document.querySelectorAll(seletor)
+        tik = true
+        vez.textContent = 'X'
+        vez.style.color = 'red'
+        array.forEach(el => {
+            if (el.textContent != '') {
+                el.textContent = ''
+            }
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function verificarVencedor() {
+    const linhasVencedoras = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let linha of linhasVencedoras) {
+        const [a, b, c] = linha;
+        if (tabuleiro[a] !== '' && tabuleiro[a] === tabuleiro[b] && tabuleiro[a] === tabuleiro[c]) {
+            console.log(tabuleiro[a])// Retorna 'X' ou 'O' (o jogador vencedor)
+        }
+    }
+
+    // return null; // Retorna null se não houver vencedor
+}
+
+
